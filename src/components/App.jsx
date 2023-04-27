61acc54775fe9d3252a86a2f2f4e03a5560aa834
NikitaDima <onekill13@gmail.com>
2023-04-27 22:08:17 +0300

    осталось добавить поисковик

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,22 @@ import ContactList from './contactList/ContactList';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
+  };
+
+  formSubmitHendler = newContacts => {
+    const { contacts } = this.state;
+    const isNameExists = contacts.some(
+      contact => contact.name.toLowerCase() === newContacts.name.toLowerCase()
+    );
+    if (isNameExists) {
+      alert(`${newContacts.name}is already in contacts`);
+      return;
+    }
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContacts],
+    }));
   };
 
   deleteContact = contactId => {
@@ -20,17 +29,29 @@ export class App extends Component {
     }));
   };
 
+  changeFilter = event => {
+    this.setState({ filter: event.currentTurget.value });
+  };
+  getFilterContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLocaleLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normalizedFilter)
+    );
+  };
+
   render() {
-    const { contacts } = this.state;
+    const { filter } = this.state;
+
     return (
       <>
         <div>
           <h1>Phonebook</h1>
-          <ContactForm></ContactForm>
+          <ContactForm onSubmit={this.formSubmitHendler}></ContactForm>
           <h2>Contacts</h2>
-          <Filter />
+          <Filter value={filter} onChange={this.changeFilter} />
           <ContactList
-            contacts={contacts}
+            contacts={this.getFilterContacts}
             onDeleteContact={this.deleteContact}
           />
         </div>
